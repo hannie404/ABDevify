051b09d46471467a3b024953995a8305d523157f
Prince Cipriano <princetonjudecipriano.com>
2023-08-21 16:43:05 +0800

    updated folder structure

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./home/Home"
+export { default as Career } from "./career/Career"
+export { default as Service } from "./service/Service"
+export { default as About } from "./about/About"
+export { default as Contact } from "./contact/Contact"
