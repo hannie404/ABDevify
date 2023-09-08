50862102792985dc1f78354787634e00ea5816d6
Prince <princetonjudecipriano@gmail.com>
2023-09-08 09:18:52 +0800

    added service + about

--- src/pages/home/sections/Hero.jsx
+++ src/pages/home/sections/Hero.jsx
@@ -2,7 +2,7 @@ import { heroImage } from '../../../assets/images';
 
 const Hero = () => {
     return (
-        <section className="relative overflow-hidden px-8 py-16 before:absolute before:left-0 before:top-0 before:h-[750px] before:w-[750px] before:-translate-x-[20%] before:content-[url(/src/assets/images/multicircle.png)] after:absolute after:right-0 after:top-0 after:-z-10 after:h-[1399.298px] after:w-[467px] after:-translate-y-[25%] after:rotate-[65.712deg] after:rounded-[233.5px] after:bg-gradient-to-b after:from-[rgba(251,168,28,0.11)] after:to-[rgba(224,86,136,0.06)] after:blur-[150px] after:content-[''] lg:py-24">
+        <section className="relative overflow-hidden px-8 py-16 lg:py-24">
             <div className="container mx-auto my-auto">
                 <div className="grid gap-4 md:grid-cols-2 md:grid-rows-[auto_minmax(0,_1fr)] md:gap-6 lg:gap-8">
                     <div className="flex flex-col gap-4 text-center md:row-start-1 md:row-end-2 md:text-start">
