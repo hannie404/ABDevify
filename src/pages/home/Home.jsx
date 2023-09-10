aa865d7aa31259bacd10cb31e05997056aa6ace1
hannie404 <annie.business04@gmail.com>
2023-09-10 08:39:26 -1200

    added a section to the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import Hero from './sections/Hero';
+import BePart from './sections/BePart';
+import MeetTheTeam from './sections/MeetTheTeam';
+
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <MeetTheTeam />
+            <BePart />
         </>
     );
 };
---
+++ src/pages/home/sections/MeetTheTeam.jsx
@@ -0,0 +1,24 @@
+const MeetTheTeam = () => {
+  return (
+      <div className="max-w-full py-10 lg:py-16">
+        <div className="container mx-auto grid gap-24 lg:grid-cols-2">
+          <div className="col-span-1 font-bold ">
+              <div className="flex justify-center lg:justify-start">
+                  <div className="top-[50%] flex  justify-center gap-8 lg:justify-start">
+                      <h2 className="text-center text-6xl uppercase leading-normal lg:text-start">
+                          Meet the{' '}
+                          <br />
+                          <span className="text-accent-400">Team</span>
+                      </h2>
+                  </div>
+              </div>
+          </div>
+        </div>
+        <div className="container mx-auto grid gap-24 lg:grid-cols-2">
+          
+        </div>
+      </div>
+    );
+};
+
+export default MeetTheTeam;
